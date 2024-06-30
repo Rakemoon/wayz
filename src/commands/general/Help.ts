5f503f35033c2131094e9267362aa566abf64bec
Rakemoon <rakemoonkuzmin@gmail.com>
2024-06-30 10:06:11 +0700

    feat: minimal implementation

---
+++ src/commands/general/Help.ts
@@ -0,0 +1,11 @@
+import Command from "#wayz/lib/structures/Command";
+
+const helpCommand = new Command()
+    .setName("help")
+    .setDescription("A help commnad")
+    .addAlias("h")
+    .setExec(async msg => {
+        await msg.sock.sendMessage(msg.key.remoteJid as string, { text: msg.localize.commands.help });
+    });
+
+export default helpCommand;
