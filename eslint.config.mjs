4ce5cd9a2b346842c26aceb729d3b45cd2e1fcca
Rakemoon <rakemoonkuzmin@gmail.com>
2024-06-30 08:42:01 +0700

    chore: Add ignored dirs for eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,6 +2,12 @@ import { common, stylistic, node, modules } from "@rakemoon/eslint-config";
 import tseslint from "typescript-eslint";
 
 export default tseslint.config(
+    {
+        ignores: [
+            "**/node_modules/**",
+            "**/dist/*"
+        ]
+    },
     ...common,
     ...stylistic,
     ...modules,
